29b0623e73f4eb30f55fa79f8b2c17672aa76c11
Harryson Pointdujour <Harryson Pointdujour>
2016-11-30 18:49:36 -0500

    'validpassword and set password methods created on user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto'); //no npm install necessary
 
 var userSchema = new Schema({
   email: {
@@ -7,17 +8,31 @@ var userSchema = new Schema({
     require: true,
     unique: true
   },
-  password: {
-    type: String,
-    required: true
-  },
   age: {
     type: Number,
     min: 13,
     max: 120,
     required: true
+  },
+  hash:{
+    type: String
+  },
+  salt:{
+    type: String
   }
 });
+
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex'); // 16 byte string
+  this.hash = crypto.pbkdf22Sync(password, this.salt, 1000, 64)
+                    .toString('hex');
+};
+userSchema.methods.validPassword = function(password){
+  var hash = crypto.pbkdf22Sync(password, this.salt, 1000, 64)
+                   .toString('hex');
+  return this.hash === hash; // if equal then correct pass has been provided
+};
+userSchema.methods.generateJwt = function(){};
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
