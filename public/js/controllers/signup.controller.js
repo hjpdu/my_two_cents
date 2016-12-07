dc71c1da3d6619b6e0e96919361ae0cbe840fbb8
Harryson Pointdujour <Harryson Pointdujour>
2016-12-06 19:33:08 -0500

    implemented signup view and functionality with basic validation and light error handling

--- public/js/controllers/signup.controller.js
+++ public/js/controllers/signup.controller.js
@@ -2,21 +2,20 @@
   angular.module('two-cents')
     .controller('SignUpController', SignUpController);
 
-  SignUpController.$inject = ['$scope', 'AuthService'];
+  SignUpController.$inject = ['$scope', 'AuthService', '$location'];
 
-  function SignUpController($scope, AuthService){
+  function SignUpController($scope, AuthService, $location){
     $scope.signup = signup;
     $scope.required = true;
 
     function signup(newUser){
-      console.log($scope.signupForm.input);
       AuthService.signup(newUser)
         .then(function(){
-          alert('Success!');
+          $location.path('/login');
         })
         .catch(function(){
           $scope.newUser = {};
-          alert('Sorry :(. Try again!');
+          $location.path('/signup');
         });
     }
 
